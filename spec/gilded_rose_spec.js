f69b2aafd0028dbee5dcebe0a3c7336698ad0571
alstadler00 <alstadler.email@gmail.com>
2024-06-03 21:02:28 +0200

    added test cases for backstage passes

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -42,4 +42,32 @@ describe("Gilded Rose", function() {
     expect(items[0].quality).toEqual(80);
   });
 
+  it("should increase quality of 'Backstage passes' by 1 when more than 10days left", function() {
+    items = [ new Item("Backstage passes to a TAFKAL80ETC concert", 12, 20) ];
+    update_quality();
+    expect(items[0].sell_in).toEqual(11);
+    expect(items[0].quality).toEqual(21);
+  });
+
+  it("should increase quality of 'Backstage passes' by 2 when 10days or less left", function() {
+    items = [ new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20) ];
+    update_quality();
+    expect(items[0].sell_in).toEqual(9);
+    expect(items[0].quality).toEqual(22);
+  });
+
+  it("should increase quality of 'Backstage passes' by 3 when 5days or less left", function() {
+    items = [ new Item("Backstage passes to a TAFKAL80ETC concert", 5, 20) ];
+    update_quality();
+    expect(items[0].sell_in).toEqual(4);
+    expect(items[0].quality).toEqual(23);
+  });
+
+  it("should drop quality of 'Backstage passes' to 0 when concert is over", function() {
+    items = [ new Item("Backstage passes to a TAFKAL80ETC concert", 0, 20) ];
+    update_quality();
+    expect(items[0].sell_in).toEqual(-1);
+    expect(items[0].quality).toEqual(0);
+  });
+
 });
