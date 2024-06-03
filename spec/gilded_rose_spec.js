5cf6f5c81184e69b2ff99bff7f8c6d39e0f2b6b4
alstadler00 <alstadler.email@gmail.com>
2024-06-03 20:44:16 +0200

    fixed test case for normal items

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -3,8 +3,8 @@ describe("Gilded Rose", function() {
   it("should decrease sell_in and quality by 1 for normal items", function() {
     items = [ new Item("+5 Dexterity Vest", 10, 20) ];
     update_quality();
-    expect(items[0].sell_in).toEqual("9");
-    expect(items[0].name).toEqual("19");
+    expect(items[0].sell_in).toEqual(9);
+    expect(items[0].quality).toEqual(19);
   });
-  
+
 });
